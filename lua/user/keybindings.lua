639195bec79563cf998580006d232a6f7f166bb3
elliotxx <951376975@qq.com>
2024-04-15 11:46:05 +0800

    fix: unwork auto-save plugin and adjust key-binding

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -7,22 +7,22 @@ M.config = function()
     lvim.keys.normal_mode["|"] = ":vsplit<CR>"
     lvim.keys.normal_mode["-"] = ":split<CR>"
 
-    -- Better window movement
-    -- lvim.keys.normal_mode["<C-H>"] = "<C-w>h"
-    -- lvim.keys.normal_mode["<C-J>"] = "<C-w>j"
-    -- lvim.keys.normal_mode["<C-K>"] = "<C-w>k"
-    -- lvim.keys.normal_mode["<C-L>"] = "<C-w>l"
+    -- Better window navigation in alacritty
+    -- lvim.keys.normal_mode["<m-h>"] = "<C-w>h"
+    -- lvim.keys.normal_mode["<m-j>"] = "<C-w>j"
+    -- lvim.keys.normal_mode["<m-k>"] = "<C-w>k"
+    -- lvim.keys.normal_mode["<m-l>"] = "<C-w>l"
+    -- lvim.keys.normal_mode["<C-j>"] = "5j"
+    -- lvim.keys.normal_mode["<C-k>"] = "5k"
+    -- lvim.keys.normal_mode["<C-h>"] = "5h"
+    -- lvim.keys.normal_mode["<C-l>"] = "5l"
 
-    -- Better window navigation
-    lvim.keys.normal_mode["<m-h>"] = "<C-w>h"
-    lvim.keys.normal_mode["<m-j>"] = "<C-w>j"
-    lvim.keys.normal_mode["<m-k>"] = "<C-w>k"
-    lvim.keys.normal_mode["<m-l>"] = "<C-w>l"
-
+    -- Better window navigation in iterm2
+    lvim.keys.normal_mode["<C-h>"] = "<C-w>h"
     lvim.keys.normal_mode["<C-j>"] = "5j"
     lvim.keys.normal_mode["<C-k>"] = "5k"
-    lvim.keys.normal_mode["<C-h>"] = "5h"
-    lvim.keys.normal_mode["<C-l>"] = "5l"
+    lvim.keys.normal_mode["<C-l>"] = "<C-w>l"
+
     lvim.keys.normal_mode["H"] = "^"
     lvim.keys.normal_mode["L"] = "$"
 end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -27,7 +27,7 @@ M.config = function()
         -- Forked from auto-save.nvim as active development has stopped
         {
             "okuuva/auto-save.nvim",
-            cmd = "ASToggle", -- optional for lazy loading on command
+            -- cmd = "ASToggle", -- optional for lazy loading on command
             -- event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
             opts = {
                 -- your config goes here
@@ -275,7 +275,7 @@ M.config = function()
             },
         },
 
-        {"soulis-1256/hoverhints.nvim"},
+        { "soulis-1256/hoverhints.nvim" },
     }
 end
 
