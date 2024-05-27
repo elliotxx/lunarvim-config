ba5a4f9b2068a72298e321ee931e3e0dffac1b5b
elliotxx <951376975@qq.com>
2024-05-27 17:50:50 +0800

    refactor: optimize keymapping for float terminal

--- lua/user/terminal.lua
+++ lua/user/terminal.lua
@@ -4,10 +4,10 @@ M.config = function()
     lvim.builtin.terminal.active = true
     lvim.builtin.terminal.open_mapping = [[<C-\>]]
     lvim.builtin.terminal.execs[1] = { vim.o.shell, "<C-`>", "Horizontal Terminal", "horizontal", 0.3 }
-    -- To map <Esc> to exit terminal-mode:
-    --   :tnoremap <Esc> <C-\><C-n>
+    -- To map jk to exit terminal-mode:
+    --   :tnoremap jk <C-\><C-n>
     -- You can see :help terminal
-    vim.keymap.set('t', '<ESC>', [[<C-\><C-n>]])
+    vim.keymap.set('t', 'jk', [[<C-\><C-n>]])
 end
 
 return M
