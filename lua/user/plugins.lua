6547a72e2abf9f06a390f547f1f24e701bd2a771
elliotxx <951376975@qq.com>
2024-05-29 18:13:37 +0800

    refactor: extract copy selector as custom command in neo-tree

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -356,6 +356,40 @@ M.config = function()
                 "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
             },
             opts = {
+                commands = {
+                    -- Reference: https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370#discussioncomment-6679447
+                    copy_selector = function(state)
+                        -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+                        -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+                        local node = state.tree:get_node()
+                        local filepath = node:get_id()
+                        local filename = node.name
+                        local modify = vim.fn.fnamemodify
+
+                        local results = {
+                            filepath,
+                            modify(filepath, ':.'),
+                            modify(filepath, ':~'),
+                            filename,
+                            modify(filename, ':r'),
+                            modify(filename, ':e'),
+                        }
+
+                        vim.ui.select({
+                            '1. Absolute path: ' .. results[1],
+                            '2. Path relative to CWD: ' .. results[2],
+                            '3. Path relative to HOME: ' .. results[3],
+                            '4. Filename: ' .. results[4],
+                            '5. Filename without extension: ' .. results[5],
+                            '6. Extension of the filename: ' .. results[6],
+                        }, { prompt = 'Choose to copy to clipboard:' }, function(choice)
+                            local i = tonumber(choice:sub(1, 1))
+                            local result = results[i]
+                            vim.fn.setreg("+", result)
+                            vim.notify('Copied: ' .. result)
+                        end)
+                    end,
+                },
                 filesystem = {
                     filtered_items = {
                         hide_dotfiles = false,
@@ -373,38 +407,7 @@ M.config = function()
                     },
                     window = {
                         mappings = {
-                            -- Reference: https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/370#discussioncomment-6679447
-                            ['Y'] = function(state)
-                                -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
-                                -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
-                                local node = state.tree:get_node()
-                                local filepath = node:get_id()
-                                local filename = node.name
-                                local modify = vim.fn.fnamemodify
-
-                                local results = {
-                                    filepath,
-                                    modify(filepath, ':.'),
-                                    modify(filepath, ':~'),
-                                    filename,
-                                    modify(filename, ':r'),
-                                    modify(filename, ':e'),
-                                }
-
-                                vim.ui.select({
-                                    '1. Absolute path: ' .. results[1],
-                                    '2. Path relative to CWD: ' .. results[2],
-                                    '3. Path relative to HOME: ' .. results[3],
-                                    '4. Filename: ' .. results[4],
-                                    '5. Filename without extension: ' .. results[5],
-                                    '6. Extension of the filename: ' .. results[6],
-                                }, { prompt = 'Choose to copy to clipboard:' }, function(choice)
-                                    local i = tonumber(choice:sub(1, 1))
-                                    local result = results[i]
-                                    vim.fn.setreg("+", result)
-                                    vim.notify('Copied: ' .. result)
-                                end)
-                            end
+                            ['Y'] = "copy_selector",
                         },
                     },
                 },
