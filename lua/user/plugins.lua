8282900288879843309c9fedfb818faa6972acee
elliotxx <951376975@qq.com>
2024-11-28 21:29:54 +0800

    fix: glitchy cursor while changing line on sphamba/smear-cursor.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -20,18 +20,10 @@ M.config = function()
                 -- your config goes here
                 -- or just leave it empty :)
                 -- enabled = true, -- start auto-save when the plugin is loaded (i.e. when your package manager loads it)
-                execution_message = {
-                    enabled = true,
-                    message = function() -- message to print on save
-                        return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
-                    end,
-                    dim = 0.18,                                    -- dim the color of `message`
-                    cleaning_interval = 1250,                      -- (milliseconds) automatically clean MsgArea after displaying `message`. See :h MsgArea
-                },
                 trigger_events = {                                 -- See :h events
                     immediate_save = { "BufLeave", "FocusLost" },  -- vim events that trigger an immediate save
                     defer_save = { "InsertLeave", "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
-                    cancel_defered_save = { "InsertEnter" },       -- vim events that cancel a pending deferred save
+                    cancel_deferred_save = { "InsertEnter" },      -- vim events that cancel a pending deferred save
                 },
                 -- function that takes the buffer handle and determines whether to save the current buffer or not
                 -- return true: if buffer is ok to be saved
